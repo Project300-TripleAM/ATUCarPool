16f2c323a0e44bc86c1c8cd27c047accfffaa4d0
Adam Grace <S00188523@atu.ie>
2023-11-28 23:41:52 +0000

    routing fixed & errorMessage on invalid details

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,16 +11,18 @@ export class LoginComponent {
 
   username!: string;
   password!: string;
+  errorMessage: string = '';
 
   constructor(private authService: AuthService,private router: Router) {}
 
   login() {
     if (this.authService.login(this.username, this.password)) {
-
       // Redirect to the dashboard
       console.log('Login successful');
       this.router.navigate(['/dashboard']);
     } else {
+      // Display error message
+      this.errorMessage = 'Invalid username or password';
       console.log('Login failed');
     }
   }
@@ -28,5 +30,13 @@ export class LoginComponent {
   register() {
     //registration logic once we implement regestration form logic
     console.log('Redirecting to registration page');
+    this.router.navigate(['/registration']);
+  }
+
+  clearFields() {
+    // Clears username and password field only if details are invalid
+    this.username = '';
+    this.password = '';
+    this.errorMessage = '';
   }
 }
