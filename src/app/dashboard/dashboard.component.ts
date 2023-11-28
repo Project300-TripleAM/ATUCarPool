596b7e91650cef4cb2bec72766b710689932757f
Marko1738 <S00153062@atu.ie>
2023-11-28 16:29:27 +0000

    Map with interactive routes added x2

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+//couldnt find google namespacework around
+declare var google: any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private directionsService: any;
+  private directionsRenderer: any;
+  private selectedRoute: any;
+  private map: any;
+  private marker: any;
+  private apiKey = environment.apiKey;
 
+  ngOnInit() {
+    (window as any).initMap = this.initMap.bind(this);
+
+    this.loadGoogleMapsScript();
+  }
+
+  loadGoogleMapsScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&libraries=geometry&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  }
+  initMap(): void {
+    const initialPosition = { lat: 54.278422, lng: -8.460434 };
+
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: initialPosition,
+    });
+
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      position: initialPosition,
+      title: 'ATU, Sligo'
+    });
+
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer({ map: this.map, suppressMarkers: true });
+
+    // route data
+    const routes = [
+      { id: 1, origin: 'Bury St, Ballina, Co. Mayo, F26 K597', destination: 'Ash Ln, Ballytivnan, Sligo' },
+      { id: 2, origin: 'Providence Rd, Foxford, Co. Mayo, F26 H9E4', destination: 'Ash Ln, Ballytivnan, Sligo' },
+    ];
+
+    //Draw routes on the map
+    routes.forEach(route => {
+      this.drawRoute(route);
+    });
+
+
+  }
+
+  drawRoute(route: { id: number, origin: string, destination: string }): void {
+    const request = {
+      origin: route.origin,
+      destination: route.destination,
+      travelMode: 'DRIVING'
+    };
+
+    this.directionsService.route(request, (result: { routes: { overview_path: any; }[]; }, status: string) => {
+      if (status === 'OK') {
+        const routePolyline = new google.maps.Polyline({
+          path: result.routes[0].overview_path,
+          strokeColor: '#FF0000', // Choose a color for the route
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        routePolyline.setMap(this.map);
+
+        //Add a click event listener to the polyline
+        routePolyline.addListener('click', () => {
+          this.selectedRoute = route;
+          this.displaySelectedRoute();
+        });
+      }
+    });
+  }
+  displaySelectedRoute(): void {
+    // Implement logic to display details of the selected route
+    console.log('Selected Route:', this.selectedRoute);
+  }
 }
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiKey: 'AIzaSyDppxsE_LSUfA4GUsCSHhcI6rCLf1I1NLI',
+  }
