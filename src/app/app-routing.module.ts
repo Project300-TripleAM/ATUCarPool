da90e32943d5031860c88bab3b0b81e3852fc791
Aiden Farrell <S00226489@atu.ie>
2024-02-07 13:54:09 +0000

    removed last of login and registration components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { MyAccountComponent } from './my-account/my-account.component'
-import { RegistrationComponent } from './registration/registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './services/authguard.service';
 import { FaqComponent } from './faq/faq.component';
 
-//Declare routing for app
+// Declare routing for app
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'my-account', component: MyAccountComponent,canActivate: [AuthGuard] },
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, //boolean canactivate to determine if this route can be accessed
-  { path: 'faq', component: FaqComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'my-account', component: MyAccountComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'faq', component: FaqComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { RegistrationComponent } from './registration/registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthService } from './services/auth.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -16,7 +14,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { MyAccountComponent } from './my-account/my-account.component';
 import { FaqComponent } from './faq/faq.component'; 
 
@@ -24,8 +21,6 @@ import { FaqComponent } from './faq/faq.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent,
     DashboardComponent,
     MyAccountComponent,
     NavbarComponent,
