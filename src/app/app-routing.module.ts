6b44c322d2203319c16a894766b9ddeb5e7898ee
Aiden Farrell <S00226489@atu.ie>
2024-03-04 19:33:27 +0000

    fixed Logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { MyAccountComponent } from './my-account/my-account.component';
+import { FaqComponent } from './faq/faq.component';
 
 // Declare routing for app
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, }, //boolean canactivate to determine if this route can be accessed
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'account', component: MyAccountComponent }
+  { path: 'account', component: MyAccountComponent },
+  { path: 'faq', component: FaqComponent }
 ]
 
 @NgModule({
