49921323f6a1778aaa8f74a1f9e449eb201f1667
S00188523 <S00188523@atu.ie>
2024-03-04 22:53:06 +0000

    map component added to routing module & my-account updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { MyAccountComponent } from './my-account/my-account.component';
+import { MapComponent } from './map/map.component'
 import { FaqComponent } from './faq/faq.component';
 
 // Declare routing for app
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, }, //boolean canactivate to determine if this route can be accessed
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'account', component: MyAccountComponent },
+  { path: 'my-account', component: MyAccountComponent },
+  { path: 'map', component: MapComponent },
   { path: 'faq', component: FaqComponent }
 ]
 
